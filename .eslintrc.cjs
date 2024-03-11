0708fe170afe1d733108fc912c940e4181067ced
Einar Logi Pétursson <einarlogip@outlook.com>
2024-03-11 20:32:27 +0000

    nota cors fyrir v4

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,38 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: ['airbnb-base', 'prettier'],
-    parserOptions: {
-      ecmaVersion: 13,
-      sourceType: 'module',
-    },
-    rules: {
-      'no-restricted-syntax': 0,
-      'import/prefer-default-export': 0,
-      'import/no-extraneous-dependencies': 0,
-      'no-param-reassign': 0,
-      'parser': '@typescript-eslint/parser',
-      quotes: ['error', 'single'],
-      'no-console': [
-        'warn',
-        {
-          allow: ['warn', 'error', 'info', 'group', 'groupCollapsed', 'groupEnd'],
-        },
-      ],
-      'import/extensions': 0,
-      'max-len': [
-        'warn',
-        {
-          code: 160,
-          ignoreUrls: true,
-          ignoreComments: true,
-          ignoreTemplateLiterals: true,
-        },
-      ],
-      'no-continue': 0,
-      'no-plusplus': 0
-    },
-  };
-  
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-restricted-syntax': 0,
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': 0,
+    'no-param-reassign': 0,
+    'parser': '@typescript-eslint/parser',
+    quotes: ['error', 'single'],
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'error', 'info', 'group', 'groupCollapsed', 'groupEnd'],
+      },
+    ],
+    'import/extensions': 0,
+    'max-len': [
+      'warn',
+      {
+        code: 160,
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    'no-continue': 0,
+    'no-plusplus': 0
+  },
+};
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { catchErrors } from './lib/catch-errors.js';
 import { router, index } from './routes/api.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const { PORT: port = 3000 } = process.env;
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 app.get('/', catchErrors(index));
 app.use(router);
